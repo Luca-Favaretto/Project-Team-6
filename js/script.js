03531a82d9a566dccfe5a9902e9bbb9b68dc3c65
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-10-17 16:45:24 +0200

    ..

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-const checkbox = document.getElementById("ceckbox-welcome");
-const btnProced = document.getElementById("proced-button");
-btnProced.addEventListener("click", function () {
-  checkbox.checked
-    ? (window.location.href = "./beanchmark.html")
-    : console.log("da ceckare");
-});
+// const checkbox = document.getElementById("ceckbox-welcome");
+// const btnProced = document.getElementById("proced-button");
+// btnProced.addEventListener("click", function () {
+
+//   checkbox.checked
+//     ? (window.location.href = "./beanchmark.html")
+//     : console.log("da ceckare");
+// });
 
 setInterval(function () {
   const numTimer = document.querySelector(".timer h2");
