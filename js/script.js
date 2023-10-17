a5759c4ab59d34922d4da8d519625dde64320e79
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-10-17 11:44:04 +0200

    ...

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// welcome
+// const checkbox = document.getElementById("ceckbox-welcome");
+// const btnProced = document.getElementById("proced-button");
+// btnProced.addEventListener("click", function () {
+//   checkbox.checked
+//     ? (window.location.href = "benckmarck-page.html")
+//     : console.log("da ceckare");
+// });
+
+// feedback
+
+const starContainer = document.getElementById("star-container");
+console.log("starContainer");
+const starImg = document.getElementByClass("star");
+console.log("starImg");
