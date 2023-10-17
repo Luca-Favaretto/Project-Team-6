620334112a91d2b9be95c90dffb0f55f81c1a32b
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-10-17 17:44:22 +0200

    ..

--- js/script.js
+++ js/script.js
@@ -1,55 +1,31 @@
-// setInterval(function () {
-//   console.log("Questa funzione verrà eseguita ogni 2000 ms.");
-//   const numTimer = document.querySelector(".timer h2");
-
-//   console.log(numTimer);
-//   if (numTimer.innerText === "0") {
-//     clearInterval(interval);
-//     window.location.href = "#";
-//   }
-
-//   numTimer.innerText--;
-// }, 1000);
-
-function updateTimerAndChart() {
+setInterval(function () {
+  console.log("Questa funzione verrà eseguita ogni 2000 ms.");
   const numTimer = document.querySelector(".timer h2");
-  numTimer.innerText--;
 
+  console.log(numTimer);
   if (numTimer.innerText === "0") {
     clearInterval(interval);
-    window.location.href = "#"; // Esegui azioni quando il timer raggiunge 0
+    window.location.href = "#";
   }
 
-  // Aggiorna i dati del grafico a torta
-  let data = {
-    labels: ["Rimanente", "Passato"],
+  numTimer.innerText--;
+}, 1000);
+
+let myCanvas = document.getElementById("myPieChart").getContext("2d");
+let myLabels = ["Rimanente", "Passato"];
+let myDati = [numTimer.innerText, 60 - numTimer.innerText];
+
+let chart = new Chart(myCanvas, {
+  type: "doughnut",
+  data: {
+    labels: myLabels,
     datasets: [
       {
-        data: [numTimer.innerText, 60 - numTimer.innerText], // Aggiorna il grafico con il tempo rimanente
-        backgroundColor: ["red", "green"]
+        label: "time",
+        data: myDati,
+        backroundColor: ["red", "green"]
       }
     ]
-  };
-  myPieChart.data = data;
-  myPieChart.update();
-}
-
-// Inizializza il grafico a torta con dati iniziali
-var ctx = document.getElementById("myPieChart").getContext("2d");
-var data = {
-  labels: ["Rimanente", "Passato"],
-  datasets: [
-    {
-      data: [60, 0], // 60 secondi iniziali rimanenti
-      backgroundColor: ["red", "green"]
-    }
-  ]
-};
-
-var myPieChart = new Chart(ctx, {
-  type: "pie",
-  data: data
+  },
+  option
 });
-
-// Aggiorna i dati ogni secondo
-var interval = setInterval(updateTimerAndChart, 1000);
