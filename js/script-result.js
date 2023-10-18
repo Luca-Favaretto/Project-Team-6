b92417da4cf5d719184fa3098c4ff1d98c991b38
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-10-18 17:59:32 +0200

    result in facimento

--- js/script-result.js
+++ js/script-result.js
@@ -4,3 +4,23 @@ console.log(reteUstBtn);
 reteUstBtn.onclick = function () {
   window.location.href = "feedback.html";
 };
+
+let countPoint = 3;
+
+//grazie al count poit in entrata il grafico a torta viene modificato di conseguenza
+const donutSegment = document.querySelector(".donut-segment1");
+countPoint *= 10;
+let wrongAnswer = 100 - countPoint;
+donutSegment.setAttribute("stroke-dasharray", `${countPoint} ${wrongAnswer}`);
+
+const correctPercent = document.getElementById("correct");
+const correctPercent1 = document.getElementById("correct1");
+
+correctPercent.innerText = countPoint + "%";
+correctPercent1.innerText = countPoint / 10 + "/10";
+
+const wrongPercent = document.getElementById("wrong");
+const wrongPercent1 = document.getElementById("wrong1");
+
+wrongPercent.innerText = wrongAnswer + "%";
+wrongPercent1.innerText = wrongAnswer / 10 + "/10";
