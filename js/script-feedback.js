ac96b66b7698d5614bca75234e3d60f0b1a3b110
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-10-20 11:28:19 +0200

    correzione ortografica

--- js/script-feedback.js
+++ js/script-feedback.js
@@ -3,7 +3,7 @@ console.log(starContainer);
 const starImg = Array.from(document.getElementsByClassName("star"));
 console.log(starImg);
 
-starImg.forEach((img) => {
+starImg.forEach(img => {
   img.addEventListener("click", function (e) {
     const indexImg = starImg.indexOf(e.target);
 
@@ -25,23 +25,27 @@ btnMoreInfo.onclick = function (e) {
   );
 };
 
-const formfeedback = document.getElementById("form-feedback").addEventListener("submit", function (e) {
-  e.preventDefault();
+const formfeedback = document
+  .getElementById("form-feedback")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  const feedbackMain = document.getElementById("feedback-main");
+    const feedbackMain = document.getElementById("feedback-main");
 
-  const numStarGray = Array.from(document.getElementsByClassName("star-gray")).length;
-  const faccina = document.createElement("p");
-  const titolo = document.createElement("h1");
-  titolo.classList.add("h1Feedback");
-  if (numStarGray < 2) {
-    console.log(numStarGray + " 9-10");
-    while (feedbackMain.firstChild) {
-      feedbackMain.removeChild(feedbackMain.firstChild);
-    }
+    const numStarGray = Array.from(
+      document.getElementsByClassName("star-gray")
+    ).length;
+    const faccina = document.createElement("p");
+    const titolo = document.createElement("h1");
+    titolo.classList.add("h1Feedback");
+    if (numStarGray < 2) {
+      console.log(numStarGray + " 9-10");
+      while (feedbackMain.firstChild) {
+        feedbackMain.removeChild(feedbackMain.firstChild);
+      }
 
-    titolo.innerText = "Siamo estiasiati per averti soddisfatto";
-    faccina.innerHTML = `
+      titolo.innerText = "Siamo estasiati per averti soddisfatto";
+      faccina.innerHTML = `
       <img
       
       
@@ -54,15 +58,15 @@ const formfeedback = document.getElementById("form-feedback").addEventListener("
       />
     `;
 
-    feedbackMain.appendChild(titolo);
-    feedbackMain.appendChild(faccina);
-  } else if (numStarGray < 4) {
-    console.log(numStarGray + " 8-7");
-    while (feedbackMain.firstChild) {
-      feedbackMain.removeChild(feedbackMain.firstChild);
-    }
-    titolo.innerText = "Grazie per il tuo feedback";
-    faccina.innerHTML = `
+      feedbackMain.appendChild(titolo);
+      feedbackMain.appendChild(faccina);
+    } else if (numStarGray < 4) {
+      console.log(numStarGray + " 8-7");
+      while (feedbackMain.firstChild) {
+        feedbackMain.removeChild(feedbackMain.firstChild);
+      }
+      titolo.innerText = "Grazie per il tuo feedback";
+      faccina.innerHTML = `
       <img
         src="https://emojiisland.com/cdn/shop/products/Slightly_Smiling_Face_Emoji_87fdae9b-b2af-4619-a37f-e484c5e2e7a4_large.png?v=1571606036" width="350
         alt="faccina sorridente"
@@ -72,15 +76,15 @@ const formfeedback = document.getElementById("form-feedback").addEventListener("
     margin-left: 20%;"
       />
     `;
-    feedbackMain.appendChild(titolo);
-    feedbackMain.appendChild(faccina);
-  } else if (numStarGray < 5) {
-    console.log(numStarGray + " 6");
-    while (feedbackMain.firstChild) {
-      feedbackMain.removeChild(feedbackMain.firstChild);
-    }
-    titolo.innerText = "cercheremo di migliorare";
-    faccina.innerHTML = `
+      feedbackMain.appendChild(titolo);
+      feedbackMain.appendChild(faccina);
+    } else if (numStarGray < 5) {
+      console.log(numStarGray + " 6");
+      while (feedbackMain.firstChild) {
+        feedbackMain.removeChild(feedbackMain.firstChild);
+      }
+      titolo.innerText = "cercheremo di migliorare";
+      faccina.innerHTML = `
       <img
       
       src="https://www.freeiconspng.com/uploads/sad-emoji-png-8.png" width="350"
@@ -91,15 +95,15 @@ const formfeedback = document.getElementById("form-feedback").addEventListener("
     margin-left: 20%;"
       />
     `;
-    feedbackMain.appendChild(titolo);
-    feedbackMain.appendChild(faccina);
-  } else {
-    console.log(numStarGray + " <6");
-    while (feedbackMain.firstChild) {
-      feedbackMain.removeChild(feedbackMain.firstChild);
-    }
-    titolo.innerText = "Ci dispiace non averti soddisfatto";
-    faccina.innerHTML = `
+      feedbackMain.appendChild(titolo);
+      feedbackMain.appendChild(faccina);
+    } else {
+      console.log(numStarGray + " <6");
+      while (feedbackMain.firstChild) {
+        feedbackMain.removeChild(feedbackMain.firstChild);
+      }
+      titolo.innerText = "Ci dispiace non averti soddisfatto";
+      faccina.innerHTML = `
       <img
       src="https://www.freeiconspng.com/uploads/crying-emoji-png-10.png" width="350"
         alt="crying emoji png"
@@ -109,7 +113,7 @@ const formfeedback = document.getElementById("form-feedback").addEventListener("
     margin-left: 20%;"
       />
     `;
-    feedbackMain.appendChild(titolo);
-    feedbackMain.appendChild(faccina);
-  }
-});
+      feedbackMain.appendChild(titolo);
+      feedbackMain.appendChild(faccina);
+    }
+  });
